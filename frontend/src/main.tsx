343cf7e72d607974ffa1336261d274714b3f352d
SavvySID <siddhant3187@gmail.com>
2025-09-02 14:19:53 +0530

    chore: update frontend/src/main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,23 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { WagmiProviderWrapper } from './providers/WagmiProvider'
+import { RainbowKitProviderWrapper } from './providers/RainbowKitProvider'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster 
-        position="top-right"
-        toastOptions={{
-          duration: 4000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
-        }}
-      />
-    </BrowserRouter>
+    <WagmiProviderWrapper>
+      <RainbowKitProviderWrapper>
+        <BrowserRouter>
+          <App />
+          <Toaster 
+            position="top-right"
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: '#363636',
+                color: '#fff',
+              },
+            }}
+          />
+        </BrowserRouter>
+      </RainbowKitProviderWrapper>
+    </WagmiProviderWrapper>
   </React.StrictMode>,
 )
